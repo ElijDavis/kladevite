e035f46020e3d2b610bc71047b3b35593a7b47b4
ElijDavis <elijahadavis@hotmail.com>
2026-09-12 09:31:29 -0400

    temp remove app

--- src/app.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", textAlign: "center", marginTop: "100px" }}>
-      <h1>🚀 Vite + React Live on Klade!</h1>
-      <p>Static assets compiled successfully and routed via global CDN edge nodes.</p>
-    </div>
-  );
-}
